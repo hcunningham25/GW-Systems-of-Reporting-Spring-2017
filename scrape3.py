1f91d0cf5dc58e0cc30f31b1a066b8905fe22ce3
hcunningham25 <hanna25411@gwmail.gwu.edu>
2017-03-28 10:39:39 -0400

    Add files via upload

---
+++ scrape3.py
@@ -0,0 +1,26 @@
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'https://columbian.gwu.edu/2015-2016'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html).encode('utf-8')
+table = soup.find('table')
+
+list_of_rows = []
+counter = 1
+for row in table.findAll('tr')[1:-1]:
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        if cell.find('href'):
+            list_of_cells.append("https://columbian.gwu.edu/2015-2016" + cell.find('href')['href'])
+        else:
+            list_of_cells.append(cell.text)
+    list_of_rows.append(list_of_cells)
+
+outfile = open("grants2.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
+writer.writerows(list_of_rows)
