672dd3934c266ce267ac9f104d55818442cfb02a
hcunningham25 <hanna25411@gwmail.gwu.edu>
2017-03-28 09:38:52 -0400

    Add files via upload

--- scrape2.py
+++ scrape2.py
@@ -1,3 +1,4 @@
+
 import csv
 import requests
 from BeautifulSoup import BeautifulSoup
@@ -14,7 +15,11 @@ counter = 1
 for row in table.findAll('tr')[1:-1]:
     list_of_cells = []
     for cell in row.findAll('td'):
-		list_of_rows.append(list_of_cells)
+        if cell.find('href'):
+            list_of_cells.append("https://columbian.gwu.edu/2015-2016" + cell.find('href')['href'])
+        else:
+            list_of_cells.append(cell.text)
+    list_of_rows.append(list_of_cells)
 
 outfile = open("grants1.csv", "wb")
 writer = csv.writer(outfile)
