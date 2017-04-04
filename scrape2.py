29e335542968cd9075a1e629b4fcca62e79d5441
hcunningham25 <hanna25411@gwmail.gwu.edu>
2017-04-03 23:23:06 -0400

    Add files via upload

--- scrape2.py
+++ scrape2.py
@@ -1,27 +1,30 @@
-
 import csv
 import requests
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup 
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
-url = 'https://columbian.gwu.edu/2015-2016'
-response = requests.get(url)
+
+response = requests.get("http://m.nationals.mlb.com/roster/transactions/2017/03")
 html = response.content
 
 soup = BeautifulSoup(html)
 table = soup.find('table')
 
 list_of_rows = []
-counter = 1
-for row in table.findAll('tr')[1:-1]:
-    list_of_cells = []
-    for cell in row.findAll('td'):
-        if cell.find('href'):
-            list_of_cells.append("https://columbian.gwu.edu/2015-2016" + cell.find('href')['href'])
+for row in table.findAll('tr')[1:]:
+        list_of_cells = []
+
+        for cell in row.findAll('td'):
+      		if cell.find('a'):
+           		 list_of_cells.append("http://m.nationals.mlb.com/roster/transactions/2017/03" + cell.find('a')['href'])
         else:
             list_of_cells.append(cell.text)
-    list_of_rows.append(list_of_cells)
-
-outfile = open("grants1.csv", "wb")
+            list_of_cells.append(cell.text.encode('utf-8'))
+        list_of_rows.append(list_of_cells)
+     
+outfile = open("transactions2.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["Department", "Faculty", "Sponsor", "Title"])
+writer.writerow(["URL","Text","Date"])
 writer.writerows(list_of_rows)
